f2246bf2198310451151c0384a5829cecea07cd4
wistromh04 <wherfordt@gmail.com>
2024-07-01 20:05:10 -0500

    made calculate function not use eval(), complete project

--- script.js
+++ script.js
@@ -8,13 +8,55 @@ function clearDisplay(){
 display.value = "";
 }
 
-function calculate(){
-    try {
-        display.value = eval(display.value);
-    } catch(error) {
-        display.value = "ERR";
+function calculate() {
+    let expression = display.value;
+
+    // Regular expression to match operators (+, -, *, /)
+    let operators = /[+\-*\/]/g;
+
+    // Split the expression based on operators
+    let parts = expression.split(operators);
+
+    // Extract the operators from the expression
+    let operatorsArray = expression.match(operators);
+
+    // Convert parts to numbers
+    let numbers = parts.map(parseFloat);
+
+    // Perform calculations
+    let result = numbers[0];
+
+    for (let i = 0; i < operatorsArray.length; i++) {
+        let operator = operatorsArray[i];
+        let number = numbers[i + 1];
+
+        switch (operator) {
+            case '+':
+                result += number;
+                break;
+            case '-':
+                result -= number;
+                break;
+            case '*':
+                result *= number;
+                break;
+            case '/':
+                if (number !== 0) {
+                    result /= number;
+                } else {
+                    display.value = "0";
+                    return;
+                }
+                break;
+            default:
+                break;
+        }
     }
+
+    // Update the display with the result
+    display.value = result;
 }
+
 
 function backspace(){
     let currentDisplayValue = display.value;
